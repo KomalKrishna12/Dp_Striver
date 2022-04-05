b6682805b77be9a33a4d18bf0298fa00881dbe1b
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-05 22:03:39 +0530

    fibonacci number

--- Q1_Fibonacci.java
+++ Q1_Fibonacci.java
@@ -38,5 +38,15 @@ public class Q1_Fibonacci{
         // now make prev1 as prev2, prev1 as curri
         // prev2 = prev1, prev1 = curri
         // so at end curri will got at n+1 and in prev1 we have stored our ans for nth factoria;
+
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curri = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curri;
+        }
+        System.out.println(prev1);
     }
 }
