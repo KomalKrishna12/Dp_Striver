301fa57fe3a4fa13bcc48cb1d9cda58e5eb206a8
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-05 21:34:16 +0530

    fibonacci

--- Q1_Fibonacci.java
+++ Q1_Fibonacci.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class Q1_Fibonacci{
 
+    // memoization
+    // we have an array dp[n] in which by default fill with -1
+    // if n is less than equal to 1 then return n
+    // if dp[n] is not equal to -1 that means we already have that fibonacci
+    // at end return for n-1 and n-2
     public static int fun(int n, int[] dp){
         if(n <= 1) return n;
         if(dp[n] != -1) return dp[n];
