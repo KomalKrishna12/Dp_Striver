0ccf330d8d29ffe08239aa3194fcf8467f1de1cf
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-06 13:58:42 +0530

    frog jump with k distance

---
+++ Q4_Frog_jump_with_k_distance.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Q4_Frog_jump_with_k_distance {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] heights = new int[n];
+        for(int i = 0; i < n; i++) heights[i] = scn.nextInt();
+        scn.close();
+    }
+}
