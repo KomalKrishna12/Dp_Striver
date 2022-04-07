11c80183b87cf4cb482344ae5dea77813501bf4d
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-07 08:44:23 +0530

    frog jump with k distance

--- Q4_Frog_jump_with_k_distance.java
+++ Q4_Frog_jump_with_k_distance.java
@@ -1,11 +1,32 @@
 import java.util.*;
 public class Q4_Frog_jump_with_k_distance {
+    public static int solve(int n, int k, int[] dp){
+        if(n == 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+        int minStep = Integer.MAX_VALUE;
+        for(int i = 1; i <= k; i++){
+            if(n-i >= 0){
+            int jump = solve(n - i, k, dp) + Math.abs(dp[n] - dp[n-i]);
+            minStep = Math.min(jump, minStep);
+            }
+        }
+        return dp[n] = minStep;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+        int n = scn.nextInt(); // total no of steps
 
         int[] heights = new int[n];
-        for(int i = 0; i < n; i++) heights[i] = scn.nextInt();
+        for(int i = 0; i < n; i++) heights[i] = scn.nextInt(); // energy required on n steps
+        int k = scn.nextInt(); // jumps which are possibls
         scn.close();
+
+        // memoization
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        System.out.println(solve(n-1, k, dp));
+
     }
 }
