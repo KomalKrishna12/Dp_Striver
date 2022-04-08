7c9168034b69c9cf93848c0d8ad8ac86be6a8ba8
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-08 10:58:03 +0530

    ninjas training

--- Q7_Ninjas_training.java
+++ Q7_Ninjas_training.java
@@ -60,17 +60,17 @@ public class Q7_Ninjas_training {
 
         // now tabulation method
         int[][] dp = new int[n][4];
-        dp[0][0] = Math.max(dp[0][1], dp[0][2]);
-        dp[0][1] = Math.max(dp[0][0], dp[0][2]);
-        dp[0][2] = Math.max(dp[0][0], dp[0][1]);
-        dp[0][3] = Math.max(dp[0][0], Math.max(dp[0][1], dp[0][2]));
+        dp[0][0] = Math.max(points[0][1], points[0][2]);
+        dp[0][1] = Math.max(points[0][0], points[0][2]);
+        dp[0][2] = Math.max(points[0][0], points[0][1]);
+        dp[0][3] = Math.max(points[0][0], Math.max(points[0][1], points[0][2]));
 
         for(int day = 1; day < n; day++){
             for(int last = 0; last < 4; last++){
                 dp[day][last] = 0;
                 for(int task = 0; task < 3; task++){
                     if(task != last){
-                        int point = points[day][task] + dp[day - 1][task];
+                        int point = points[day][task] + dp[day - 1][task]; 
                         dp[day][last] = Math.max(dp[day][last], point);
                     }
                 }
