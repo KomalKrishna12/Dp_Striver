6add935c3409626216eac920a58666470018d6d0
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-17 18:42:51 +0530

    unbounded knapsack

---
+++ Q23_Unbounded_knapsack.java
@@ -0,0 +1,70 @@
+// this is similar like zero one knapsack but here for every weights we have 
+// infinite supply so we can use same idx many times
+public class Q23_Unbounded_knapsack {
+    public static int f(int idx, int W, int[] wt, int[] val) {
+
+        if (idx == 0)
+            return (W / wt[idx]) * val[idx];
+
+        int nottake = 0 + f(idx - 1, W, wt, val);
+
+        int take = Integer.MIN_VALUE;
+        if (wt[idx] <= W)
+            take = val[idx] + f(idx, W - wt[idx], wt, val);
+
+        return Math.max(take, nottake);
+
+    }
+
+    public static void main(String[] args) {
+
+        int wt[] = { 2, 4, 6 };
+        int val[] = { 5, 11, 13 };
+        int W = 10;
+
+        int n = wt.length;
+
+        // recursion
+        // System.out.println(f(n-1, W, wt, val));
+
+        // memoization
+        int[][] dp = new int[n][W + 1];
+
+        for (int i = 0; i <= W; i++)
+            dp[0][i] = (i / wt[0]) * val[0];
+
+        for (int idx = 1; idx < n; idx++) {
+
+            for (int tar = 0; tar <= W; tar++) {
+
+                int nottake = 0 + dp[idx - 1][tar];
+
+                int take = Integer.MIN_VALUE;
+                if (wt[idx] <= tar)
+                    take = val[idx] + dp[idx][tar - wt[idx]];
+
+                dp[idx][tar] = Math.max(take, nottake);
+
+            }
+
+        }
+
+        System.out.println(dp[n - 1][W]);
+
+    }
+
+    public static int f2(int idx, int W, int[] wt, int[] val) {
+
+        if (idx == 0)
+            return (W / wt[idx]) * val[idx];
+
+        int nottake = 0 + f2(idx - 1, W, wt, val);
+
+        int take = Integer.MIN_VALUE;
+        if (wt[idx] <= W)
+            take = val[idx] + f2(idx, W - wt[idx], wt, val);
+
+        return Math.max(take, nottake);
+
+    }
+}
