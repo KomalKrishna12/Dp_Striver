2db43b1557fb29999eface5af00ac8072fe4ea56
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-19 22:11:59 +0530

    shortest common supersequence

--- Q31_Shortest_common_supersequence.java
+++ Q31_Shortest_common_supersequence.java
@@ -1,5 +1,14 @@
+// in this problem we have to print the shortest common supersequence, it is like union of two strings
+// so we find out the longest common subsequence then the length of supersubsequence will be n + m - lcs
+// we subtract lcs bcoz we don't want to take longest common subsequence two times
+// now in order to print run a loop from i = n and j = m if both are greater than 0 then check
+// char at i and j (1 based indexing so s1.charAt(i-1)) if they are same then add ith char into ans string
+// and decrease both i and j
+// if (i-1)th cell is greater than print ith char and set i-- else do for j
+// after while loop their is chance that any string is rest so check for s1 and s2 at end return ans
 public class Q31_Shortest_common_supersequence {
     public static void main(String[] args) {
+
         String s1 = "brute";
         String s2 = "groot";
 
