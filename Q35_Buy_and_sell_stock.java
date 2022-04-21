5daa65e79376204dea800a553099fd7453b36069
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-21 07:19:34 +0530

    buy and sell stock

--- Q35_Buy_and_sell_stock.java
+++ Q35_Buy_and_sell_stock.java
@@ -1,7 +1,7 @@
 // given an array which denotes we have diff rate in diff days
 // we want to buy and sell the stock with maximum profit or the best time to but and sell
 // to get max profit we have to buy the stock in min price and sell it on high
-public class Q35_But_and_sell_stock{
+public class Q35_Buy_and_sell_stock{
     public static void main(String[] args) {
         int[] arr = {7, 1, 5, 3, 6, 4};
 
