1f92fbc33ea84cd4a03d0c004742ca58b4c328e9
KomalKrishna12 <komalkrishna016@gmail.com>
2022-04-21 17:54:47 +0530

    buy and sell stock II

--- Q36_Buy_and_sell_stock_II.java
+++ Q36_Buy_and_sell_stock_II.java
@@ -46,8 +46,34 @@ public class Q36_Buy_and_sell_stock_II {
         // System.out.println(f(0, 1, arr, n));
 
         // memoization
-        Integer[][] dp = new Integer[n+1][2];
-        System.out.println(f2(0, 1, arr, n, dp));
+        // Integer[][] dp = new Integer[n+1][2];
+        // System.out.println(f2(0, 1, arr, n, dp));
+
+        // tabulation
+        int[][] dp = new int[n+1][2];
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int i = n-1; i >= 0; i--){
+
+            for(int j = 0; j < 2; j++){
+
+                int profit = 0;
+
+                if(j == 1){
+                    profit = Math.max(-arr[i] + dp[i+1][0], dp[i+1][1]);
+                }
+                else{
+                    profit = Math.max(arr[i] + dp[i+1][1], dp[i+1][0]);
+                }
+
+                dp[i][j] = profit;
+
+            }
+
+        }
+
+        System.out.println(dp[0][1]);
 
     }
 
