853988fc3972d7a8e4421079aa514678ba875b6b
KomalKrishna12 <komalkrishna016@gmail.com>
2022-05-31 17:00:58 +0530

    pallindrome partitioning II

--- Q51_Pallindrome_partioning_II.java
+++ Q51_Pallindrome_partioning_II.java
@@ -9,31 +9,35 @@
 // if that string is pallindrome then add 1 for the cut and call the function afain for j+1 to n
 // and using a min variable find out the mincut
 public class Q51_Pallindrome_partioning_II {
-    public static boolean isPallindrome(int i, int j, String s){
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
+    public static boolean isPallindrome(int i, int j, String s) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
             i++;
             j--;
         }
         return true;
     }
-    public static int f(int i, int n, String s){
-        if(i == n) return 0;
+
+    public static int f(int i, int n, String s) {
+        if (i == n)
+            return 0;
 
         int min = Integer.MAX_VALUE;
 
-        for(int j = i; j < n; j++){
-            if(isPallindrome(i, j, s)){
-                int cuts = 1 + f(j+1, n, s);
+        for (int j = i; j < n; j++) {
+            if (isPallindrome(i, j, s)) {
+                int cuts = 1 + f(j + 1, n, s);
                 min = Math.min(min, cuts);
             }
         }
 
         return min;
     }
+
     public static void main(String[] args) {
         String str = "aab";
-        int n = str.length() - 1; 
-        System.out.println(f(0, n-1, str));
+        int n = str.length() - 1;
+        System.out.println(f(0, n - 1, str));
     }
 }
