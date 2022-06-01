7d46432530ae1d6b434eb028f5eb32f1151f9b91
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-01 17:30:07 +0530

    count square submatrix of all ones

--- Q54_Count_square_submatrix_of_all_ones.java
+++ Q54_Count_square_submatrix_of_all_ones.java
@@ -1,3 +1,9 @@
+// given a 2d matrix we have to count total no of square submatrix of all ones
+// if element is 1 that means it is a 1*1 square submatrix so we have to count all 
+// for first row and column it'll be equal to their matrix element
+// from (1,1) compare three values (i-1, j), (i, j-1) and (i-1, j-1) and take min of all and add 1
+// and store
+// if matrix[i][j] == 0 that means no square possible using (i, j) so store 0
 public class Q54_Count_square_submatrix_of_all_ones {
     public static void main(String[] args) {
         int[][] matrix = {
