078127d25c4e771f26700b93d7dc74279e59e275
KomalKrishna12 <komalkrishna016@gmail.com>
2022-06-01 17:27:18 +0530

    count square submatrix of all ones

--- Q54_Count_square_submatrix_of_all_ones.java
+++ Q54_Count_square_submatrix_of_all_ones.java
@@ -1,33 +1,36 @@
 public class Q54_Count_square_submatrix_of_all_ones {
     public static void main(String[] args) {
         int[][] matrix = {
-                { 0, 1, 1, 1 },
-                { 1, 1, 1, 1 },
-                { 0, 1, 1, 1 }
+                { 1, 0, 1 },
+                { 1, 1, 0 },
+                { 1, 1, 0 }
         };
         int n = matrix.length;
         int m = matrix[0].length;
 
         int[][] dp = new int[n][m];
 
-        for(int j = 0; j < m; j++){
+        for (int j = 0; j < m; j++) {
             dp[0][j] = matrix[0][j];
         }
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             dp[i][0] = matrix[i][0];
         }
 
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < m; j++){
-                dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0)
+                    dp[i][j] = matrix[i][j];
+                else
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
             }
         }
 
         int count = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 count += dp[i][j];
             }
         }
