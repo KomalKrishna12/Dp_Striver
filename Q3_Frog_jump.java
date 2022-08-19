6eb90f109b101c2270b3f960538b50d1c9733269
KomalKrishna12 <komalkrishna016@gmail.com>
2022-08-19 20:31:53 +0530

    dp

--- Q3_Frog_jump.java
+++ Q3_Frog_jump.java
@@ -1,8 +1,8 @@
 import java.util.*;
-// in the qustion we have given n staricase and height array which contains energy at each staris
+// in the qustion we have given n staricase and height array which contains energy at each stairs
 // their is frog who can jump either 1 step or 2 step and lost some energy
 // if frog go to jth stair from ith stair then energy used by frog will be abs of height[j] - height[i]
-// so frog want to go to nth satir by using min energy
+// so frog want to go to nth stair by using min energy
 public class Q3_Frog_jump {
 
     public static int solve(int n, int[] dp, int[] heights){
