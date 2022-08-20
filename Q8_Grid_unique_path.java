39c6f7c26f3c5932295be3574bdc7df57e5b274f
KomalKrishna12 <komalkrishna016@gmail.com>
2022-08-20 12:04:53 +0530

    dp

--- Q8_Grid_unique_path.java
+++ Q8_Grid_unique_path.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-
 // in this question we have given a m*n grid we are at top left and we've to reach bottom right
 // we can mode in two directions either right or down
 // we have to find total no of unique paths to reach the destination
@@ -19,11 +17,11 @@ public class Q8_Grid_unique_path {
         int n = scn.nextInt();
         scn.close();
         // recursion
-        // pass m-1 and n-1 in ith and jth index and start travelling through destination which is (0,0)
+        // pass m-1 and n-1 in ith and jth index and start travelling for destination which is (0,0)
         // if i and j is at 0 then return 1 that means we got one path
-        // if any one going outside the boundary then return 0 that means no result
-        // now call for up in up row will decrease by 1
-        // call for left in left col will decrease by 1
+        // if any one going outside the boundary then return 0 that means no paths
+        // now call for up, in up row will decrease by 1
+        // call for left, in left col will decrease by 1
         // at end return addition of both bcoz we can get path in up also and in left also
         // System.out.println(helper(m-1, n-1));
 
